aea73658531250235734ce96e342976ea5a11713
MorgBeka <morgbeka@student.arteveldehs.be>
2019-03-01 17:28:44 +0100

    Mario

---
+++ mario.py
@@ -0,0 +1,24 @@
+from sense_hat import SenseHat
+
+sense = SenseHat()
+
+rood = [255,0,0]
+bruin = [153,76,0]
+huid = [255,204,153]
+
+geel = [255,255,0]
+blauw = [0,0,255]
+grijs = [224,224,24]
+
+marioDown = [
+    
+    
+    ]
+
+
+
+marioJump = [
+    
+    ]
+
+
